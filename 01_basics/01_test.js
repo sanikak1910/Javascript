42c68ab640989ad6d37a2c9285c588f2c1971268
sanikak1910 <142019273+sanikak1910@users.noreply.github.com>
2025-04-26 17:11:12 +0000

    Memory types

--- 01_basics/01_test.js
+++ 01_basics/01_test.js
@@ -2,3 +2,8 @@
 
 // alert("Hello"); //we are uing nodejs not browser so it will not work
 
+//Stack Memory and Heap Memory 
+//Stack Memory: stores primitive values like numbers, strings, booleans, null, undefined
+//Heap Memory: stores reference values like objects, arrays, functions
+//Stack Memory is faster than Heap Memory
+//Stack Memory is used for static memory allocation and Heap Memory is used for dynamic memory allocation
