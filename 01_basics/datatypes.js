382b23e3acb6c7328dae98a0153f3235a04a5fa6
sanikak1910 <142019273+sanikak1910@users.noreply.github.com>
2025-04-26 16:21:42 +0000

    Conversion Of Datatypes

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -12,3 +12,42 @@ console.log(typeof state); //object
 console.log(typeof "Denmark"); //string
 console.log(typeof null); //object
 console.log(typeof undefined); //undefined
+
+//Conversion of Datatypes
+//1.Convert to Number
+let str = "123";
+let valueInNumber = Number(str); //123
+console.log(typeof valueInNumber);
+console.log(typeof(+str)); //number
+let str1 = "123abc";
+let valueInNumber1 = Number(str1); //NaN
+console.log(typeof valueInNumber1); //number
+console.log(valueInNumber1); //NaN
+
+let str3 = null;
+let valueInNumber3 = Number(str3);
+console.log(typeof valueInNumber3);//number
+console.log(valueInNumber3); //0
+//undefined: NaN(not a number)
+//null:0
+//boolean: true :1 , false:0
+//String: NaN
+//Number: value 
+
+// 2. Converting number to Boolean
+let isLoggedIn = 0;
+let booleanLoggedIn = Boolean(isLoggedIn);
+console.log(booleanLoggedIn); //false
+
+// 1 => true, 0 => false
+// "" => false
+//"string" => true
+
+// 3. Converting number to String
+let num = 123;
+let num1=88;
+let strNum = String(num);
+console.log(strNum); //123
+console.log(typeof strNum); //string
+console.log(typeof(num1+"")); //string
+console.log(num1+"");
