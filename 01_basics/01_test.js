b9d1864465b44101eefc91714c157f2c7680acbc
sanikak1910 <142019273+sanikak1910@users.noreply.github.com>
2025-04-30 15:33:56 +0000

    Basic of Javascript

--- 01_basics/01_test.js
+++ 01_basics/01_test.js
@@ -6,4 +6,4 @@
 //Stack Memory: stores primitive values like numbers, strings, booleans, null, undefined
 //Heap Memory: stores reference values like objects, arrays, functions
 //Stack Memory is faster than Heap Memory
-//Stack Memory is used for static memory allocation and Heap Memory is used for dynamic memory allocation
+//Stack Memory is used for static memory allocation and Heap Memory is used for dynamic memory allocation 
